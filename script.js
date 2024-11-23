e09ee6331078f7b4bbf5e83d1d1e609e1b1cf912
Candace <csun0@terpmail.umd.edu>
2024-11-23 00:59:41 -0500

    draft cont i may have done this for like too many hours straight

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let imgs = ["title-page-draft.gif", "2.gif", "3.gif"];
+let img_nums = [];
 let currIndex = 0;
 
+for (let i = 1; i <= 21; i++) {
+  img_nums.push("slides/" + i + ".gif");
+}
+
+console.log(img_nums);
+
 function nextPage() {
-  if (currIndex < imgs.length - 1) {
+  if (currIndex < img_nums.length - 1) {
     currIndex += 1;
   }
   document.getElementById(
     "bg"
-  ).style.backgroundImage = `url('${imgs[currIndex]}')`;
+  ).style.backgroundImage = `url('${img_nums[currIndex]}')`;
 }
 
 function prevPage() {
@@ -16,7 +23,7 @@ function prevPage() {
   }
   document.getElementById(
     "bg"
-  ).style.backgroundImage = `url('${imgs[currIndex]}')`;
+  ).style.backgroundImage = `url('${img_nums[currIndex]}')`;
 }
 
 window.addEventListener("mousemove", (e) => {
@@ -26,3 +33,39 @@ window.addEventListener("mousemove", (e) => {
   const light = document.getElementById("light");
   light.style.transform = `translate(${x - 6}px, ${y - 6}px)`;
 });
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+  switch (event.key) {
+    case "ArrowLeft":
+      // Left pressed
+      prevPage();
+      break;
+    case "ArrowRight":
+      // Right pressed
+      nextPage();
+      break;
+    case "ArrowUp":
+      // Up pressed
+      break;
+    case "ArrowDown":
+      // Down pressed
+      break;
+  }
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
